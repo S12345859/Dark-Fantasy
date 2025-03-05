dfa3806c5a6c1016b4dc9361799dd211655931c9
𝘿𝙖𝙧𝙠 𝙁𝙖𝙣𝙩𝙖𝙨𝙮 👑 <rik.sardar111@gmail.com>
2025-03-05 12:58:17 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Dark ',
+  author: 'Fantasy',
+  botName: "Dark Fantasy",
+  botOwner: 'Dark Fantasy', // Your name
+  ownerNumber: '919382758804', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
